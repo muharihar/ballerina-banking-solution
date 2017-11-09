6953b160c9b134d943c08dcdd5281baea1df449f
dilinisg_89 <dilinisg@gmail.com>
2017-11-09 13:02:33 +0530

    Update the db schema

--- resources/db-schema.sql
+++ resources/db-schema.sql
@@ -63,3 +63,42 @@ CREATE TABLE IF NOT EXISTS OTP_Info(
     PRIMARY KEY(id),
     FOREIGN KEY (user_id) REFERENCES Customer_Info(user_id) ON DELETE CASCADE
 )
+
+
+INSERT INTO `Bank`.`Account_Type` (`account_type_id`, `account_name`, `interest_rate`) VALUES ('1', 'Hapan', '5');
+INSERT INTO `Bank`.`Account_Type` (`account_type_id`, `account_name`, `interest_rate`) VALUES ('2', 'Ithuru Mithuru', '4.5');
+
+INSERT INTO `Bank`.`Currency` (`currency_id`, `currency_name`) VALUES ('1', 'LKR');
+INSERT INTO `Bank`.`Currency` (`currency_id`, `currency_name`) VALUES ('2', 'USD');
+INSERT INTO `Bank`.`Currency` (`currency_id`, `currency_name`) VALUES ('3', 'AUD');
+INSERT INTO `Bank`.`Currency` (`currency_id`, `currency_name`) VALUES ('4', 'CAD');
+INSERT INTO `Bank`.`Currency` (`currency_id`, `currency_name`) VALUES ('5', 'INR');
+
+INSERT INTO `Bank`.`Account` (`acc_number`, `created_date`, `current_balance`, `user_id`, `account_type_id`, `currency_id`) VALUES ('0117654326', '2005/11/02', '129000.89', '1034567890', '1', '1');
+INSERT INTO `Bank`.`Account` (`acc_number`, `created_date`, `current_balance`, `user_id`, `account_type_id`, `currency_id`) VALUES ('0114565456', '2010/04/03', '350344.78', '1067544678', '2', '1');
+INSERT INTO `Bank`.`Account` (`acc_number`, `created_date`, `current_balance`, `user_id`, `account_type_id`, `currency_id`) VALUES ('0117654356', '2016/09/23', '234888.67', '1076699776', '1', '2');
+
+INSERT INTO `Bank`.`Customer_Info` (`user_id`, `first_name`, `last_name`, `national_id`, `birth_date`, `email`, `address`) VALUES ('1067544678', 'Dilini', 'Gunatilake', '897645244V', '1989/05/01', 'dilinisg@gmai.com', '15C, Rathmalana');
+INSERT INTO `Bank`.`Customer_Info` (`user_id`, `first_name`, `last_name`, `national_id`, `birth_date`, `email`, `address`) VALUES ('1034567890', 'Chathurika', 'De Silva', '867865344V', '1986/07/23', 'chathurika@gmail.com', 'No.12,Maradana');
+INSERT INTO `Bank`.`Customer_Info` (`user_id`, `first_name`, `last_name`, `national_id`, `birth_date`, `email`, `address`) VALUES ('1076699776', 'Yasassri', 'Rathnayake', '897637266V', '1989/05/31', 'yasassri@gmai.com', 'No.67/23, Kottawa');
+
+INSERT INTO `Bank`.`OTP_Info` (`id`, `otp_id`, `created_date`, `user_id`) VALUES ('1', '6789', '2017/11/01', '1067544678');
+INSERT INTO `Bank`.`OTP_Info` (`id`, `otp_id`, `created_date`, `user_id`) VALUES ('2', '3456', '2017/11/10', '1034567890');
+INSERT INTO `Bank`.`OTP_Info` (`id`, `otp_id`, `created_date`, `user_id`) VALUES ('3', '4532', '2017/11/03', '1076699776');
+
+INSERT INTO `Bank`.`Pay_Orders` (`pay_order_id`, `transaction_amount`, `transaction_date`, `acc_number`, `to_acc_number`, `frequency`) VALUES ('1230', '1000.00', '2019-01-01 10:00:01.000000', '114565456', '117654326', '1');
+INSERT INTO `Bank`.`Pay_Orders` (`pay_order_id`, `transaction_amount`, `transaction_date`, `acc_number`, `to_acc_number`, `frequency`) VALUES ('1231', '2500.00', '2018-12-01 11:00:01.000000', '117654356', '116786547', '1');
+
+INSERT INTO `Bank`.`Utility_Provider` (`utility_provider_id`, `utlity_provider_name`) VALUES ('1', 'HSBC');
+INSERT INTO `Bank`.`Utility_Provider` (`utility_provider_id`, `utlity_provider_name`) VALUES ('2', 'Mobitel');
+INSERT INTO `Bank`.`Utility_Provider` (`utility_provider_id`, `utlity_provider_name`) VALUES ('3', 'Dialog');
+INSERT INTO `Bank`.`Utility_Provider` (`utility_provider_id`, `utlity_provider_name`) VALUES ('4', 'Water Board');
+
+INSERT INTO `Bank`.`Transactions` (`transaction_id`, `transaction_amount`, `transaction_date`, `utility_provider_id`, `acc_number`, `currency_id`) VALUES ('1000', '2350.00', '2017/11/05', '1', '114565456', '1');
+INSERT INTO `Bank`.`Transactions` (`transaction_id`, `transaction_amount`, `transaction_date`, `utility_provider_id`, `acc_number`, `currency_id`) VALUES ('1001', '5000.50', '2017/10/23', '2', '117654326', '1');
+INSERT INTO `Bank`.`Transactions` (`transaction_id`, `transaction_amount`, `transaction_date`, `utility_provider_id`, `acc_number`, `currency_id`) VALUES ('1002', '4567.90', '2017/09/22', '3', '117654356', '1');
+
+
+
+
+
